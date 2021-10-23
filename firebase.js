464f0f5816f17070a1a87276827689857787f470
Alfred Rezk <73665419+AlfredRezk@users.noreply.github.com>
2021-10-22 23:49:54 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUCdAv7QgcOwimwyTzt4-b3PmwMF0E2UA",
-  authDomain: "rnapp-chat.firebaseapp.com",
-  projectId: "rnapp-chat",
-  storageBucket: "rnapp-chat.appspot.com",
-  messagingSenderId: "731032344816",
-  appId: "1:731032344816:web:99834a4927ba93fd554a35",
-  measurementId: "G-YNQ4YW1BKW"
+//  Your configurations 
 };
 
 let app;
